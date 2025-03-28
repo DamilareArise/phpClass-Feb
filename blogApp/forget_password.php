d35cecd3e77a9759940018ea1639436691f6db68
Arise Damilare <arisedamilare5@gmail.com>
2025-03-28 11:02:23 +0100

    Email setup

--- blogApp/forget_password.php
+++ blogApp/forget_password.php
@@ -2,6 +2,14 @@
 <?php 
     require_once 'database.php';
 
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    require '../vendor/autoload.php';
+
+
+    $mail = new PHPMailer(true);
+
+    $message = '';
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $email = $_POST['email'];
         if(isset($email)){
@@ -20,10 +28,40 @@
                 $stmt->execute();
                 $stmt->close();
 
-                echo "http://localhost/phpClass/blogApp/reset_password.php/?token=$token";
-            }
-            
-            
+                $to = $email;
+                $subject = 'Reset Password';
+                $body = "follow this link to reset your password http://localhost/phpClass/blogApp/reset_password.php/?token=$token";
+
+
+                // echo "http://localhost/phpClass/blogApp/reset_password.php/?token=$token";
+                // mail($to, $subject, $message);
+                // $message = 'Password reset link has been sent to your email';
+
+                try {
+                    // SMTP Configuration
+                    $mail->isSMTP();
+                    $mail->Host = 'smtp.gmail.com';
+                    $mail->SMTPAuth = true;
+                    $mail->Username = 'arisedamilare5@gmail.com';
+                    $mail->Password = 'devx jhlm hvpt bmiu'; // Use App Password
+                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                    $mail->Port = 465;
+                
+                    // Sender and Recipient
+                    $mail->setFrom('arisedamilare5@gmail.com', 'My Blog App');
+                    $mail->addAddress($email);
+                
+                    // Email Content
+                    $mail->isHTML(true);
+                    $mail->Subject = $subject;
+                    $mail->Body    = $body;
+                
+                    $mail->send();
+                    $message = 'Password reset link has been sent to your email';
+                } catch (Exception $e) {
+                    echo "Error: {$mail->ErrorInfo}";
+                }
+            } 
         }
     }
 
@@ -31,6 +69,7 @@
 
 <section>
     <div class="container col-md-5 p-3 my-4 shadow-sm">
+        <b class="text-success"><?php echo $message ?></b>
         <form action="" method="POST">
             <h4 class="text-center">Send Password reset link</h4>
             <input type="email" placeholder="Insert your email" name="email" class="form-control mb-2">
